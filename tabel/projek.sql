1522b11991d29069394c57217696d7293bd1bed8
Hossam23RPL <hossamgaming215.com>
2023-08-12 11:18:57 +0700

    tabel sql

---
+++ tabel/projek.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2023 at 06:04 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coba coba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projek`
+--
+
+CREATE TABLE `projek` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `addres` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `projek`
+--
+
+INSERT INTO `projek` (`id`, `name`, `email`, `addres`) VALUES
+(1, 'hossamgaming215', 'hossamgaming215@gmail.com', 'JL.Panji Suroso no 6a'),
+(2, 'alexandra', 'alexandra@gmail.com', 'new york'),
+(1, 'hossamgaming215', 'hossamgaming215@gmail.com', 'JL.Panji Suroso no 6a'),
+(2, 'alexandra', 'alexandra@gmail.com', 'new york');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
